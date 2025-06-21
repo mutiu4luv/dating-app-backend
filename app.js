f1ed1ee09f42547fdf7db0b310ef1a91e3e8f297
mutiu4luv <chidiemmamadu@gmail.com>
2025-06-21 16:08:36 +0100

    app

--- app.js
+++ app.js
@@ -1,62 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const { default: mongoose } = require("mongoose");
-// const cors = require("cors");
-// const memberRouter = require("./router/memberRouter.js");
-// const { mergeMembers } = require("./controller/mergingController.js");
-// const { paystackWebhookHandler } = require("./webhooks/paystack.js");
-// const subscriptionRouter = require("./router/subcriptionRouter.js");
-// const mergeRouter = require("./router/merginRouter.js");
-
-// const app = express();
-// app.use("/uploads", express.static("uploads"));
-// // app.use(cors());
-
-// const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS: " + origin));
-//     }
-//   },
-//   credentials: true, // Required if you're using cookies
-// };
-
-// app.use(express.json()); // <-- move these up!
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors(corsOptions)); // ✅ MUST COME BEFORE all route handlers
-
-// app.use("/api/user", memberRouter);
-// app.use("/api/merge", mergeRouter);
-// app.post("/api/webhook/paystack", paystackWebhookHandler);
-// app.use("/api/subscription", subscriptionRouter);
-// // app.use("/merge", mergeRouter);
-
-// app.get("/", (req, res) => {
-//   res.send("Hello Victor, welcome to whoba Ogo foundation");
-// });
-
-// app.listen(process.env.PORT, () => {
-//   console.log("server is running in port 7000");
-// });
-
-// // mongoose
-// //   .connect(process.env.MONGO_URI)
-// //   .then(() => {
-// //     console.log("database connected");
-// //   })
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     serverSelectionTimeoutMS: 90000, // 30 seconds
-//     socketTimeoutMS: 95000, // 45 seconds
-//   })
-//   .catch(() => {
-//     console.log("database not connected");
-//   });
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -92,9 +33,6 @@ io.on("connection", (socket) => {
 
   socket.on("send_message", async (data) => {
     io.to(data.room).emit("receive_message", data);
-
-    // Optionally save to DB (if model is connected)
-    // await Message.create({ ...data });
   });
 
   socket.on("disconnect", () => {
