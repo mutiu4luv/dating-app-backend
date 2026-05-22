7c664b30c2c40ff0d53580e29acb904f756c6181
mutiu4luv <chidiemmamadu@gmail.com>
2026-05-22 11:00:20 +0100

    chat route

--- router/chatRoute.js
+++ router/chatRoute.js
@@ -16,6 +16,11 @@ router.get("/", protect, getChatMessages);
 router.post("/save", protect, upload.single("image"), saveMessage);
 router.get("/conversations/:userId", protect, getUserConversations);
 router.get("/admin/activity", protect, getAdminChatActivity);
+router.get(
+  "/admin/conversation/users/:member1/:member2",
+  protect,
+  getAdminConversationMessages
+);
 router.get("/admin/conversation/:room", protect, getAdminConversationMessages);
 router.get("/unread/count/:userId", protect, getUnreadMessageCount);
 router.put("/read/:userId", protect, markMessagesAsRead);
